16afa71cb500a7985952ebaa5ae0a67e7b55333a
jo-mut <johnmutuku628@gmail.com>
2025-04-17 23:13:05 +0300

    theme provider

--- src/xtrant_crawler/components/providers/app_providers.cljs
+++ src/xtrant_crawler/components/providers/app_providers.cljs
@@ -4,43 +4,49 @@
    [reagent.core :as r]))
 
 (def initial-state
-  {:theme "system"
-   :setTheme (fn [] nil)})
+  #js {:theme "system"
+       :setTheme (fn [_] nil)})
 
-(defonce theme-context (.createContext initial-state))
+(defonce theme-context (createContext initial-state))
 
-(defn theme-provider
+(defn- f-theme-provider
   [{:keys [default-theme storage-key]
-    :as props
     :or {default-theme "system"
-         storage-key "vite-ui-theme"}} children]
-  (let [[theme set-theme] (useState (fn []
-                                      (or (.getItem js/localstorage storage-key)
-                                          default-theme)))]
+         storage-key "vite-ui-theme"} 
+    :as props}
+   children]
+  (let [[theme set-theme]
+        (useState (fn []
+                    (or (.getItem js/localStorage storage-key)
+                        default-theme)))]
 
     (useEffect
      (fn []
-       (let [root (.-documentElement (.document js/window))]
+       (let [root (.-documentElement js/document)]
          (.remove (.-classList root) "light" "dark")
-         (let [system-theme (-> js/window
-                                (.matchMedia "(prefers-color-scheme: dark)")
-                                .-matches)]
-           (if (= theme :system)
-             (.add (.-classList root) (if system-theme "dark" "light"))
-             (.add (.-classList system-theme))))))
-     [theme])
+         (let [resolved-theme (if (= theme "system")
+                                (if (.. js/window
+                                        (matchMedia "(prefers-color-scheme: dark)")
+                                        -matches)
+                                  "dark"
+                                  "light")
+                                theme)]
+           (.add (.-classList root) resolved-theme)))
+       js/undefined)
+     #js [theme])
 
-    (let [value #js {:theme theme
-                     :setTheme (fn []
-                                 (.setItem js/localstorage storage-key theme)
-                                 (set-theme theme))}]
-      [:> (.-Provider theme-context)
-       {:value value}
-       (r/as-element children)])))
+    [:> (.-Provider theme-context)
+     {:value #js {:theme theme
+                  :setTheme (fn [new-theme]
+                              (.setItem js/localStorage storage-key new-theme)
+                              (set-theme new-theme))}}
+     (r/as-element children)]))
+
+(defn provider [props children]
+  [:f> f-theme-provider props children])
 
 (defn use-theme []
   (let [context (useContext theme-context)]
     (when-not context
-      (throw (js/Error.  "useTheme must be used within a ThemeProvider")))
+      (throw (js/Error. "useTheme must be used within a ThemeProvider")))
     context))
-
